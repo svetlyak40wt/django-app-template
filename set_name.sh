ddd12088eaf2f9bf9b89d143961cd05795e3df9e
Alexander Artemenko <svetlyak.40wt@gmail.com>
2009-11-24 23:02:22 +0300

    Added shell script to change package's name in some config files.

---
+++ set_name.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECT=$1
+LOWER_PROJECT=${PROJECT/-/_}
+
+find . \
+    \( -not \( -path './parts/*' -or -path './bin/*' -or -path './develop-eggs/*' \) \) -and \
+    \( -name '*.py' -or -name '*.rst' -or -name Makefile -or -name *.cfg -or -name LICENSE -or -name .gitignore \) \
+    -print0 | \
+    xargs -0 sed -i \
+                -e "s/django-app-template/${PROJECT}/g" \
+                -e "s/django_app_template/${LOWER_PROJECT}/g"
